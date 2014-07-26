47212f31a1661dba74b23fcdee17ba3d44040eda
Alex Muscar <muscar@gmail.com>
2014-07-26 08:54:17 +0300

    Added structures--inefficient implementation. Added rem comments. A rem comment appears as a comment at the *end of the following line of emitted GCC code*. Added startup code that calls the main function.

--- compiler.lisp
+++ compiler.lisp
@@ -3,6 +3,13 @@
 
 (in-package :icfp)
 
+(defun partition (pred sequence)
+  (let (left right)
+    (dolist (e sequence (values (nreverse left) (nreverse right)))
+      (if (funcall pred e)
+	  (push e left)
+	  (push e right)))))
+
 (defparameter *gcc-program* '())
 
 ;; GCC assembly
@@ -15,7 +22,7 @@
 
 (defun compile-gcc (program)
   (multiple-value-bind (program label-addrs) (collect-labels program)
-    (write-gcc (patch-program program label-addrs))))
+    (write-gcc (patch-program program label-addrs) label-addrs)))
 
 (defun collect-labels (program)
   (let ((idx 0)
@@ -23,11 +30,10 @@
 	(label-addrs '()))
     (dolist (instr program (values (nreverse new-program)
 				   (nreverse label-addrs)))
-      (if (mark-label-p instr)
-	  (push (cons (cadr instr) idx) label-addrs)
-	  (progn
-	    (incf idx)
-	    (push instr new-program))))))
+      (cond ((mark-label-p instr) (push (cons (cadr instr) idx) label-addrs))
+	    ((remp instr) (push instr new-program))
+	    (t (incf idx)
+	       (push instr new-program))))))
 
 (defun patch-program (program label-addrs)
   (mapcar (lambda (instr)
@@ -39,16 +45,43 @@
 	      (t instr)))
 	  program))
 
-(defun write-gcc (program)
+(defun rem-instr-p (instr)
+  (and (consp instr) (eq (car instr) 'rem)))
+
+(defun ldf-instr-p (instr)
+  (and (consp instr) (eq (car instr) 'ldf)))
+
+(defun write-gcc (program label-addrs)
   (loop for instr in program
-     for idx from 0
-     do (format t "~{~a ~}~10,25@t; ~a~%" instr idx)))
+     with pending-label = nil
+     do (cond ((ldf-instr-p instr) (let ((name (car (find-if (lambda (entry)
+							       (= (cdr entry) (second instr)))
+							     label-addrs))))
+				     (format t "~{~a ~}~10,25@t; address of ~a~%" instr name)))
+	      ((remp instr) (setf pending-label (rest instr)))
+	      (t (if pending-label
+		     (progn
+		       (format t "~{~a ~}~10,25@t; ~{~a ~}~%" instr pending-label)
+		       (setf pending-label nil))
+		     (format t "~{~a ~}~%" instr))))))
 
 ;; Lang 0
 
-(defparameter *lang0-program* '((defun main ()
+;; BUG Locals don't work in the first frame -- no space
+
+(defparameter *lang0-program* '((defstruct point
+				  x y z)
+				(rem "The netry point")
+				(defun main (state)
 				  ;(min (cons 10 20))
-				  (test-loop 12))
+				  ;(test-loop 12)
+				  (test-make-struct)
+				  )
+				(defun test-make-struct ()
+				  (local p)
+				  (test-struct-field (make-struct point 10 20 30)))
+				(defun test-struct-field (p)
+				  (struct-field point z p))
 				;; (defun min (cell)
 				;;   (if (< (car cell) (cdr cell))
 				;;       (car cell)
@@ -59,14 +92,16 @@
 				;;   (set! x 10)
 				;;   (set! y 20)
 				;;   (+ x y))
-				(defun test-loop (n)
-				  (local x)
-				  (while (> n 0)
-				    (set! n (- n 1))
-				    (set! x (+ x 2)))
-				  x)))
+				;; (defun test-loop (n)
+				;;   (local x)
+				;;   (while (> n 0)
+				;;     (set! n (- n 1))
+				;;     (set! x (+ x 2)))
+				;;   x)
+				))
 
 (defparameter *functions* '())
+(defparameter *structs* '())
 (defparameter *l0-current-function* nil)
 (defparameter *l0-delayed-code* nil)
 
@@ -74,35 +109,79 @@
   name
   args
   locals
+  frame-size
   body)
 
-(defun collect-functions (program)
-  (dolist (fun program)
-    (destructuring-bind (name args &rest body) (cdr fun)
-      (assert (eq (car fun) 'defun) () "expecting function definition")
-      (push (cons name (make-l0-function :name name
-					 :args args
-					 :locals (collect-locals body)
-					 :body body)) *functions*)))
-  (setf *functions* (nreverse *functions*)))
+(defstruct l0-struct
+  name
+  fields)
+
+(defun fundefp (form)
+  (and (consp form) (eq (first form) 'defun)))
+
+(defun collect-functions (functions)
+  (setf *functions* (mapcar #'analyze-function functions)))
+
+(defun analyze-function (fun)
+  (assert (fundefp fun) () "expecting function definition")
+  (destructuring-bind (name args &rest body) (cdr fun)
+    (let ((locals (collect-locals body)))
+      (cons name (make-l0-function :name name
+				   :args args
+				   :locals locals
+				   :frame-size (+ (length args) (length locals))
+				   :body body)))))
 
 (defun collect-locals (body)
   (loop for instr in body
      when (and (consp instr) (eq (first instr) 'local))
      nconc (rest instr)))
 
+(defun collect-structs (structs)
+  (setf *structs* (mapcar #'analyze-struct structs)))
+
+(defun analyze-struct (struct)
+  (destructuring-bind (name &rest fields) (cdr struct)
+    (cons name (make-l0-struct :name name
+			       :fields (collect-fields fields)))))
+
+(defun collect-fields (fields)
+  (loop for field in fields
+     for idx from 0
+     collect (cons field idx)))
+
+(defun field-index (struct name)
+  (let ((field (assoc name (l0-struct-fields struct))))
+    (unless field
+      (error "struct ~a does not have a field ~a" (l0-struct-name struct) name))
+    (cdr field)))
+
 (defun compile-lang0 (program)
   (let ((*gcc-program* '())
-	(*functions* '()))
-    (collect-functions program)
-    (dolist (*l0-current-function* (mapcar #'cdr *functions*) (compile-gcc (nreverse *gcc-program*)))
-      (let ((*l0-delayed-code* '()))
-	(push `($mark-label ,(l0-function-name *l0-current-function*)) *gcc-program*)
-	(dolist (instr (l0-function-body *l0-current-function*))
-	  (compile-lang0-instruction instr))
-	(push '(RTN) *gcc-program*)
-	(dolist (codegen *l0-delayed-code*)
-	  (funcall codegen))))))
+	(*functions* '())
+	(*structs* '()))
+    (multiple-value-bind (functions structs) (partition #'fundefp program)
+      (collect-functions functions)
+      (collect-structs structs)
+      ;; Emit entry point
+      (let ((main-function (cdr (assoc 'main *functions*))))
+	(unless main-function
+	  (error "no main function defined"))
+	(dotimes (idx (l0-function-frame-size main-function))
+	  (lang0-emit 'ldc 0))
+	(lang0-emit 'ldf 'main)
+	(lang0-emit 'ap (l0-function-frame-size main-function))
+	(lang0-emit 'rtn))
+      
+      (dolist (*l0-current-function* (mapcar #'cdr *functions*) (compile-gcc (nreverse *gcc-program*)))
+	(let ((*l0-delayed-code* '()))
+	  (lang0-emit 'rem (l0-function-name *l0-current-function*))
+	  (push `($mark-label ,(l0-function-name *l0-current-function*)) *gcc-program*)
+	  (dolist (instr (l0-function-body *l0-current-function*))
+	    (compile-lang0-instruction instr))
+	  (push '(RTN) *gcc-program*)
+	  (dolist (codegen *l0-delayed-code*)
+	    (funcall codegen)))))))
 
 (defun compile-lang0-instruction (instr)
   (cond ((consp instr) (compile-lang0-call instr))
@@ -133,10 +212,44 @@
       (+ (length (l0-function-args *l0-current-function*))
 	 (position sym (l0-function-locals *l0-current-function*)))))
 
+;; Lang0 primitives
+
+(defun lang0-emit (opcode &rest operands)
+  (push (cons opcode operands) *gcc-program*))
+
+(defun lang0-prim-make-struct (name values)
+  (let ((struct (cdr (assoc name *structs*))))
+    (unless struct
+      (error "unknown structure ~a" name))
+    (unless (= (length (l0-struct-fields struct)) (length values))
+      (error "trying to construct ~a struct wrong number of values; expecting ~a, got ~a"
+	     name
+	     (length (l0-struct-fields struct))
+	     (length values)))
+    (dolist (value values)
+      (compile-lang0-instruction value))
+    ;; Emit NIL
+    (lang0-emit 'ldc 0)
+    (dotimes (idx (length values))
+      (lang0-emit 'cons))))
+
+(defun lang0-prim-struct-field (name field-name instance)
+  (let ((struct (cdr (assoc name *structs*))))
+    (unless struct
+      (error "unknown structure ~a" name))
+    (let ((field-index (cdr (assoc field-name (l0-struct-fields struct)))))
+      (unless field-index
+	(error "unknown field ~a" field-name))
+      (compile-lang0-instruction instance)
+      (dotimes (idx field-index)
+	(lang0-emit 'cdr))
+      (lang0-emit 'car))))
+
 (defun compile-lang0-prim (instr)
   (destructuring-bind (op &rest args) instr
     (case op
       (local t)
+      (rem (lang0-emit 'rem (first args)))
       (dbug (push '(dbug) *gcc-program*)
 	    t)
       (set! (compile-lang0-instruction (second args))
@@ -145,6 +258,10 @@
       ((atom car cdr) (compile-lang0-instruction (first args))
                       (push (list op) *gcc-program*)
                       t)
+      (make-struct (lang0-prim-make-struct (first args) (rest args))
+		   t)
+      (struct-field (lang0-prim-struct-field (first args) (second args) (third args))
+		    t)
       (if (let ((then-label (fresh-label "then"))
 		(else-label (fresh-label "else"))
 		(else-branch (third args)))
