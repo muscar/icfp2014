719878f59716d64d24e0df4e661ea86e8c5cd6aa
Laurentiu Nicola <grayshade@gmail.com>
2014-07-27 15:06:00 +0300

    Try to find cond codegen bug

--- sample.lisp
+++ sample.lisp
@@ -50,15 +50,15 @@
 ;;   (dbug (f 10)))
 
 
-;; (defun null (l)
-;;   (if (atom l)
-;;       (= l 0)
-;;       0))
+(defun null (l)
+  (if (atom l)
+      (= l 0)
+      0))
 
-;; (defun map (f xs)
-;;   (if (null xs)
-;;       xs
-;;       (cons (f (car xs)) (map f (cdr xs)))))
+(defun map (f xs)
+  (if (null xs)
+      xs
+      (cons (f (car xs)) (map f (cdr xs)))))
 
 ;; (defun fold (f xs)
 ;;   (if (atom xs)
@@ -97,21 +97,44 @@
 ;;       (foldl f (f acc (car list)) (cdr list))))
 
 (defconstant nil 0)
+(defconstant t 1)
 
-(defun null (thing)
-  (and (atom thing) (= thing 0)))
+;; (defun null (thing)
+;;   (and (atom thing) (= thing 0)))
 
-(defun reverse (list acc)
-  (if (null list)
-      acc
-      (reverse (cdr list) (cons (car list) acc))))
+;; (defun reverse (list acc)
+;;   (if (null list)
+;;       acc
+;;       (reverse (cdr list) (cons (car list) acc))))
 
-(defun filter (pred list acc)
-  (if (null list)
-      (reverse acc nil)
-      (if (pred (car list))
-	  (filter pred (cdr list) (cons (car list) acc))
-	  (filter pred (cdr list) acc))))
+;; (defun filter (pred list acc)
+;;   (if (null list)
+;;       (reverse acc nil)
+;;       (if (pred (car list))
+;; 	  (filter pred (cdr list) (cons (car list) acc))
+;; 	  (filter pred (cdr list) acc))))
+
+;; (defun foo ()
+;;   1)
+
+(defconstant wall 0)
+(defconstant empty 1)
+(defconstant pill 2)
+(defconstant power-pill 3)
+(defconstant fruit-location 4)
+(defconstant player-start-pos 5)
+(defconstant ghost-start-pos 6)
+
+(defun cell-score (cell)
+  (cond ((= cell player-start-pos) 0)
+	((= cell ghost-start-pos) -1)
+	(t cell)))
+
+(defun for-each (f xs)
+  (if (not (null xs))
+      (begin
+       (f (car xs))
+       (for-each f (cdr xs)))))
 
 (defun main ()
   ;; (dbug (fold (lambda (x y)
@@ -138,16 +161,33 @@
   ;; (dbug one)
   ;; (dbug two)
   ;; (find-if (lambda (x) (= x 7)) (list 1 2 3 4 5))
-  (local (y 42))
-  (local (f (lambda (x)
-	      (dbug y)
-	      (+ 1 1))))
-  (local (xs (filter f (list 1 2 3) 0)))
-  (dbug xs)
-
+  ;; (local (y 42))
+  ;; (local (f (lambda (x)
+  ;; 	      (dbug y)
+  ;; 	      (+ 1 1))))
+  ;; (local (xs (filter f (list 1 2 3) 0)))
+  ;; (dbug xs)
+  ;; (dbug (foo))
   ;; (while (not (null x))
   ;;   (dbug x)
   ;;   (set! x (cdr x)))
   ;; (local (x (foldl (lambda (x y) (+ x y)) 0 (list 1 2 3))))
   ;; (dbug x)
+
+  ;; (dbug (cell-score wall))
+  ;; (dbug (cell-score empty))
+  ;; (dbug (cell-score pill))
+  ;; (dbug (cell-score power-pill))
+  ;; (dbug (cell-score fruit-location))
+  ;; (dbug (cell-score player-start-pos))
+  ;; (dbug (cell-score ghost-start-pos))
+  (local (cell-types (list wall empty pill power-pill fruit-location player-start-pos ghost-start-pos)))
+  (for-each (lambda (cell)
+	      (dbug (cell-score cell)))
+	    cell-types)
+  (dbug (map cell-score cell-types))
+
+  ;; (while (not (null cell-types))
+  ;;   (dbug (cell-score (car cell-types)))
+  ;;   (set! cell-types (cdr cell-types)))
   )
